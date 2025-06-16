22a022b67bedcfffc62dbc32eb7147b4093c3945
Olivier van Helden <olivier@van-helden.net>
2025-06-16 04:42:16 +0200

    update composer packages

---
+++ cli/vendor/humbug/box/rector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withAutoloadPaths([
+        __DIR__ . '/vendor/autoload.php',
+        __DIR__ . '/vendor-bin/rector/vendor/autoload.php',
+    ])
+    ->withImportNames(removeUnusedImports: true)
+    ->withPhpSets(php82: true)
+    ->withSets([
+        PHPUnitSetList::PHPUNIT_100,
+    ])
+    ->withSkip([
+        \Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class,
+        \Rector\Php55\Rector\String_\StringClassNameToClassConstantRector::class => [
+            __DIR__.'/src/Configuration/Configuration.php',
+        ],
+        \Rector\Php73\Rector\String_\SensitiveHereNowDocRector::class,
+    ]);
